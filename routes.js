7ef729145006bb90fa0967d8460f42d3393b7402
M-Yusei1001 <bitterchocolate1001@gmail.com>
2024-08-12 03:00:10 +0000

    240812: add and delete

--- routes.js
+++ routes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const pg = require('pg');
-const { connectionString } = require('pg/lib/defaults');
+const { connectionString, client_encoding } = require('pg/lib/defaults');
 const app = express();
 
 app.use(express.static("public"))
@@ -36,45 +36,43 @@ app.get('/app1', async(req, res) => {
         const client = await mydb.connect();
         
         //接続できたらクエリを送る
-        const results = await client.query('SELECT * FROM pgpractice');
-        res.render('memo_app.ejs', {items: results.rows});
-    }catch(err){
-        console.error('Error executing query', err.stack);
-        res.status(500).send('Internal Server Error');
+        const items = await client.query(
+            'SELECT * FROM public.pgpractice'
+        );
+        console.log("取得しました");
+        console.log(items.rows);
+        res.render('memo_app.ejs', {items: items.rows});
+    }catch(e){
+        console.error(e.stack);
     }
 });
 
-app.post('/create', (req, res) =>{
-    async (resolve, reject) => {
-        //非同期処理
-        //データベースへの接続を待つ
+app.post('/create', async(req, res) =>{
+    try{
         const client = await mydb.connect();
-        
-        //接続できたらクエリを送る
-        const results = await client.query('INSERT INTO pgpractice (fruits) VALUES (?)',
-        [req.body.itemName],
-        (error, results) => {
-            if(error){
-                console.error(error);
-            }else{
-                console.log(results);
-                res.redirect('/app1');
-            }
-        });
-    };
+        await client.query(
+            "INSERT INTO public.pgpractice (fruits) VALUES ($1)",
+            [req.body.itemName]
+        );
+        console.log("追加しました:" + req.body.itemName);
+        res.redirect('/app1');
+    }catch(e){
+        console.error(e.stack);
+    }
+});
 
-    // mydb.query(
-    //     'INSERT INTO pgpractice (fruits) VALUES (?)',
-    //     [req.body.itemName],
-    //     (error, results) => {
-    //         if(error){
-    //             console.error(error);
-    //         }else{
-    //             console.log(results);
-    //             res.redirect('/app1');
-    //         }
-    //     }
-    // );
+app.post('/delete/:id', async(req, res) => {
+    try{
+        const client = await mydb.connect();
+        await client.query(
+            'DELETE FROM public.pgpractice WHERE id = ($1)',
+            [req.params.id],
+        );
+        console.log("削除しました");
+        res.redirect('/app1');
+    }catch(e){
+        console.error(e.stack);
+    }
 });
 
 app.listen(3000);
--- sql.js
+++ sql.js
@@ -6,55 +6,113 @@ const mydb = new pg.Pool({
   user: 'postgres',
   password: 'yusei1001',
 
-  //仮想環境からホストOSのSQLサーバにアクセスする場合、
-  //"host.docker.internal"でホストOSのIPを指定する。
-  //あとはdevcontainerでポートを指定する。
-
+  //仮想環境からホストOSのSQLサーバにアクセスする場合、"host.docker.internal"でホストOSのIPを指定する。
   host: 'host.docker.internal', 
+
+  //あとはdevcontainerでもポートを指定する。
   port: 5432,
 });
 
-const client = mydb.connect((err) => {
+const selectQuery = {
+  text: "SELECT * FROM public.pgpractice"
+};
+
+const insertQuery = {
+  text: "INSERT INTO public.pgpractice (fruits) VALUES ($1)",
+  values: ["みかん"]
+};
+
+mydb.connect(async (err, client) => {
   if(err){
-    console.error(err);
+    console.error(err.stack);
   }else{
-    console.log("connected");
+    console.log("接続しました");
+
+    try{
+      const items = await client.query(selectQuery);
+      console.log("取得しました");
+      console.log(items.rows);
+
+      await client.query(insertQuery);
+      console.log("挿入しました");
+
+      const updatedItems = await client.query(selectQuery);
+      console.log("取得しました");
+      console.log(updatedItems.rows);
+    }catch(e){
+      console.error("ERROR OCCURRED:", e.stack);
+    }finally{
+      client.release();
+    }
   }
 });
 
-mydb.query()
 
-// async (res, rej) => {
-//   const client = await mydb.connect((err) => {
-//     if(err){
-//       console.error(err);
-//     }
-//   });
+// mydb.connect((err, client) => {
+//   if(err){
+//     console.error(err.stack);
+//   }else{
+//     console.log("接続しました");
+//     client
+//       .query(selectQuery)
+//       .then((items) => {
+//         console.log("取得しました");
+//         console.log(items.rows);
+//       })
+//       .catch((e) => {
+//         console.error(e.stack);
+//       });
 
-//   const getFromSQL = await client.query(
-//     'SELECT * FROM pgpractice',
-//     (err, result) => {
-//       if(err){
-//         console.error(err);
-//       }else{
-//         console.log("get successed");
-//         console.log(result.rows);
-//       }
-//     }
-//   );
+//     client
+//       .query(insertQuery)
+//       .then(() => {
+//         console.log("挿入しました");
+//       })
+//       .catch((e) => {
+//         console.error(e.stack);
+//       });
 
-//   const insertToSQL = await client.query(
-//     'INSERT INTO pgpractice (fruits) VALUES (?)',
-//     ["みかん"],
-//     (err, result) => {
-//       if(err){
-//         console.error(err);
-//       }else{
-//         console.log("insert successed");
-//       }
-//     }
-//   );
+//     client
+//       .query(selectQuery)
+//       .then((items) => {
+//         console.log("取得しました");
+//         console.log(items.rows);
+//       })
+//       .catch((e) => {
+//         console.error(e.stack);
+//       });
+//   }
+// });
 
-//   getFromSQL;
+// mydb.connect((err, client) => {
+//   if(err){
+//     console.error(err.stack);
+//   }else{
+//     console.log("接続しました");
+//     client
+//       .query(insertQuery)
+//       .then(() => {
+//         console.log("挿入しました");
+//       })
+//       .catch((e) => {
+//         console.error(e.stack);
+//       });
+//   }
+// });
 
-// }
+// mydb.connect((err, client) => {
+//   if(err){
+//     console.error(err.stack);
+//   }else{
+//     console.log("接続しました");
+//     client
+//       .query(selectQuery)
+//       .then((items) => {
+//         console.log("取得しました");
+//         console.log(items.rows);
+//       })
+//       .catch((e) => {
+//         console.error(e.stack);
+//       });
+//   }
+// });
